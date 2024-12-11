a318e9ae25e39663f10982be8611d01c76f57b78
akbarbaihaqi1611 <akbarbaihaqi1611@gmail.com>
2024-12-11 22:49:38 +0000

    membuat tampilan favorite, dan memperapih tampilan lainnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:myapp/login.dart';
-import 'package:myapp/onboarding.dart';
 import 'package:myapp/Login.dart';
-import 'package:myapp/sign_up.dart';
-//import 'package:myapp/homescreen.dart';
+import 'package:myapp/onboarding.dart';
+import 'package:myapp/homescreen.dart';
 import 'package:myapp/buy.dart';
 import 'package:myapp/notif.dart';
+import 'package:myapp/favorite.dart';
 
 void main() {
   runApp(const MyApp());
